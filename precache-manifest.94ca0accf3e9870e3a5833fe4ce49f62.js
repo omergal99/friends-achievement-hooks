5678a1c7f0130adfca3781bcfe5b66c91b038bf0
omergal99 <omergal99gm@gmail.com>
2019-07-10 02:01:16 +0300

    Updates

---
+++ precache-manifest.94ca0accf3e9870e3a5833fe4ce49f62.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c16307d1a82b98d990ff031e4e061c6",
+    "url": "/friends-achievement-hooks/index.html"
+  },
+  {
+    "revision": "31b854aea8696081b7f0",
+    "url": "/friends-achievement-hooks/static/css/main.fefc599b.chunk.css"
+  },
+  {
+    "revision": "fc3964cf394ba138e6a9",
+    "url": "/friends-achievement-hooks/static/js/2.55fc596e.chunk.js"
+  },
+  {
+    "revision": "31b854aea8696081b7f0",
+    "url": "/friends-achievement-hooks/static/js/main.514fe3c7.chunk.js"
+  },
+  {
+    "revision": "0d6af1e2512175a072a9",
+    "url": "/friends-achievement-hooks/static/js/runtime~main.fef64a14.js"
+  },
+  {
+    "revision": "18440e08823f8b121c95204a15b3a085",
+    "url": "/friends-achievement-hooks/static/media/LibreBaskerville-Regular.18440e08.ttf"
+  },
+  {
+    "revision": "56cb366910aa712773f8f2f7e7f42b9f",
+    "url": "/friends-achievement-hooks/static/media/Merienda-Regular.56cb3669.ttf"
+  },
+  {
+    "revision": "70fda92429e8a5f2fbaa5f10da118c74",
+    "url": "/friends-achievement-hooks/static/media/PT_Sans-Web-Regular.70fda924.ttf"
+  },
+  {
+    "revision": "849e67957ea4dd5edb38eedb9d5743f1",
+    "url": "/friends-achievement-hooks/static/media/Quando-Regular.849e6795.ttf"
+  },
+  {
+    "revision": "3f482321becfaa40fcb19825fa717859",
+    "url": "/friends-achievement-hooks/static/media/SourceSansPro-Regular.3f482321.ttf"
+  }
+]);
